bd1e0fe806026491f3fac6e485ae1a6d29e96bed
Luan Argolo <ipsluan@icloud.com>
2021-09-04 14:10:34 -0300

    :sparkles: Added animations

--- src/components/PokemonThumbnails.js
+++ src/components/PokemonThumbnails.js
@@ -3,14 +3,21 @@ import React from 'react';
 const PokemonThumbnail = ({id, name, image, type}) => {
   const style = `thumb-container ${type}`;
 
+  const Capitalize = str => {
+    if (typeof str !== 'string') {
+      return '';
+    }
+    return str.charAt(0).toUpperCase() + str.substr(1);
+  }
+
   return(
     <div className={style}>
       <div className="number">
         <small>#0{id}</small>
       </div>
-      <img src={image} alt={name} />
+      <img className="image-pokemon" src={image} alt={name} />
       <div className="detail-wrapper">
-        <h3>{name}</h3>
+        <h3>{Capitalize(name)}</h3>
         <small>Type: {type}</small>
       </div>
     </div>
